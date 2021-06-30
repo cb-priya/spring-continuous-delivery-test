a659639b1f6d98a2dc1c0653e158d730b7a086c8
cb-priya <priya@chargebee.com>
2021-06-30 17:09:53 +0530

    test new class for javadoc

---
+++ src/main/java/com/deliverytest/controller/RolesController.java
@@ -0,0 +1,9 @@
+package com.deliverytest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/roles")
+@RestController
+public class RolesController {
+}
